2dc0a18ef98fad0d5a49d9c77075173d64039dab
Jose Aguilar <jaguilarny@gmail.com>
2022-04-25 16:48:52 -0400

    commit tree 9bbd2d1b0db760cf574fa8aaae3b0e61c4c94966

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 20
+const num4 = 8
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 1) + 1;
+const mod = 20 % 8;
+const max = Math.max(5, 10, 15, 20);
